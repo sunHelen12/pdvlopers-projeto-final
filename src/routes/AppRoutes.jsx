a991643fccd93bcfbb807c1e37c9071375bf8613
wabemusic <jvwatanabe@gmail.com>
2025-08-06 22:04:58 -0300

    feat: AppRoutes.jsx rotas

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,6 +3,10 @@ import { Home } from "../pages/Home";
 import { Clients } from "../pages/Clients";
 import { Finances } from "../pages/Finances";
 import { Login } from "../pages/Login";
+import ResetPassword  from "../pages/ResetPassword";
+import PrivateRoute from "../components/PrivateRoute";
+import ForgotPassword from "../pages/ForgotPassword";
+import Dashboard from "../pages/Dashboard";
 import { Messages } from "../pages/Messages";
 import { Rewards } from "../pages/Rewards";
 import { Signup } from "../pages/Signup";
@@ -10,16 +14,26 @@ import { Signup } from "../pages/Signup";
 export default function AppRoutes(){ 
     return(
         <BrowserRouter>
-        <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/clients" element={<Clients/>}/>
-        <Route path="/finances" element={<Finances/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/messages" element={<Messages/>}/>
-        <Route path="/rewards" element={<Rewards/>}/>
-        <Route path="/signup" element={<Signup/>}/>
-        <Route path="*" element={<Navigate to="/"/>}/>
-        </Routes>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/forgot-password" element={<ForgotPassword/>}/>
+                <Route path="/reset-password/:token" element={<ResetPassword/>}/>
+                <Route 
+                    path="/dashboard" 
+                    element={
+                        <PrivateRoute>
+                            <Dashboard/>
+                        </PrivateRoute>
+                    }
+                />
+                <Route path="/clients" element={<Clients/>}/>
+                <Route path="/finances" element={<Finances/>}/>
+                <Route path="/messages" element={<Messages/>}/>
+                <Route path="/rewards" element={<Rewards/>}/>
+                <Route path="/signup" element={<Signup/>}/>
+                <Route path="*" element={<Navigate to="/"/>}/>
+            </Routes>
         </BrowserRouter>
     )
 
